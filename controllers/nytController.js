a3e3a8e3f8720290c9e048659a35d8047315a63a
w0pal <mnaufalmaulana84@gmail.com>
2025-01-16 08:27:29 +0700

    add several endpoint

--- controllers/nytController.js
+++ controllers/nytController.js
@@ -31,11 +31,12 @@ exports.searchArticles = async (req, res) => {
     }
 };
 
+
 exports.searchArticlesByDate = async (req, res) => {
     const { query, start_date, end_date } = req.query;
 
     try {
-        const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${process.env.NYT_API_KEY}`;
+        const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}&api-key=${process.env.NYT_API_KEY};`
         const dateRange = start_date && end_date ? `&begin_date=${start_date}&end_date=${end_date}` : '';
         const response = await axios.get(url + dateRange);
         res.json(response.data);
@@ -75,6 +76,17 @@ exports.getBookReviews = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
+// const getBookReviews = async (req, res) => {
+//     const { query } = req.query;
+//     try {
+//         const response = await axios.get(`https://api.nytimes.com/svc/books/v3/lists/current/${query}.json?api-key=${process.env.NYT_API_KEY}`);
+//         res.json(response.data);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// };
+
 
 exports.getMovieReviews = async (req, res) => {
     try {
