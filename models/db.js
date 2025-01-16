21a984402eed83381b2ded152bca1eb69a6d322a
w0pal <mnaufalmaulana84@gmail.com>
2025-01-16 08:25:01 +0700

    fix database

--- models/db.js
+++ models/db.js
@@ -1,18 +1,21 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => {
-    if (err) {
-        console.error('Database connection failed: ' + err.stack);
-        return;
-    }
-    console.log('Connected to the database.');
+  if (err) {
+    console.error("Database connection failed: " + err.stack);
+    return;
+  }
+  console.log("Connected to the database.");
 });
 
 module.exports = db;
