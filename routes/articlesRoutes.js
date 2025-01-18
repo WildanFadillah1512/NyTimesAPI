ec01049cf2afd1f5b73d7928219901a26b666dcf
w0pal <mnaufalmaulana84@gmail.com>
2025-01-18 14:51:14 +0700

    ini ketinggalan satu

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { getAllArticles, addArticle, updateArticle, deleteArticle } = require('../controllers/articlesController');
+const userMiddleware = require('../middlewares/userMiddleware')
 
 const router = express.Router();
 
-router.get('/', getAllArticles);
-router.post('/', addArticle);
-router.put('/:id_article', updateArticle);  
-router.delete('/:id_article', deleteArticle);  
+router.get('/', userMiddleware, getAllArticles);
+router.post('/', userMiddleware, addArticle);
+router.put('/:id_article', userMiddleware, updateArticle);
+router.delete('/:id_article', userMiddleware, deleteArticle);
 
 module.exports = router;
