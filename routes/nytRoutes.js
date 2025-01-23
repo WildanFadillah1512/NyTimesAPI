9b693815aa8b0e2d14da4c184175dd863626d3e9
w0pal <mnaufalmaulana84@gmail.com>
2025-01-23 15:28:22 +0700

    add credentials login

--- routes/nytRoutes.js
+++ routes/nytRoutes.js
@@ -11,16 +11,17 @@ const {
 } = require('../controllers/nytController');
 
 const userMiddleware = require('../middlewares/userMiddleware')
+const credentialsMiddleware = require('../middlewares/credentialsMiddleware');
 
 const router = express.Router();
 
-router.get('/top-stories', userMiddleware, getTopStories);
-router.get('/search', userMiddleware, searchArticles);
-router.get('/search/date', userMiddleware, searchArticlesByDate);
-router.get('/top-stories/:category', userMiddleware, getTopStoriesByCategory);
-router.get('/most-shared/:period', userMiddleware, getMostSharedArticles);
-router.get('/book-reviews', userMiddleware, getBookReviews);
-router.get('/articles/section/:section', userMiddleware, getArticlesBySection);
+router.get('/top-stories', userMiddleware, credentialsMiddleware, getTopStories);
+router.get('/search', userMiddleware, credentialsMiddleware, searchArticles);
+router.get('/search/date', userMiddleware, credentialsMiddleware, searchArticlesByDate);
+router.get('/top-stories/:category', userMiddleware, credentialsMiddleware, getTopStoriesByCategory);
+router.get('/most-shared/:period', userMiddleware, credentialsMiddleware, getMostSharedArticles);
+router.get('/book-reviews', userMiddleware, credentialsMiddleware, getBookReviews);
+router.get('/articles/section/:section', userMiddleware, credentialsMiddleware, getArticlesBySection);
 
 
 module.exports = router;
